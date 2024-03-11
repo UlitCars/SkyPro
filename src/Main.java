5cc98df142fcf1687e0a4f12d88e58f03da9bded
UlitCars <zashibisf@gmail.com>
2024-03-11 17:00:25 +0300

    Lesson_3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         //Home Work 1
+        int age = 31;
+        if ( age >= 18) {
+            System.out.println("Человек совершеннолетний!");
+        } else {
+            System.out.println( "Человек не совершеннолетний!");
+        }
+
     }
 }
