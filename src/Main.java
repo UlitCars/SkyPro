5ce5169c8562e2215cea09f1553c46389c1ddda5
UlitCars <zashibisf@gmail.com>
2024-04-04 15:08:36 +0300

    Lesson_10

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,59 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
+    //Lesson_10 HomeWork_1
+    public static void printLeapYear (int years){
+        if ((years % 4 == 0 && years % 100 != 0) || years % 400 == 0){
+            System.out.println(years + " год является високосным!");
+        } else {
+            System.out.println(years + " год не является високосным.");
+        }
+    }
+    //Lesson_10 HomeWork_2
+    public static void printRequiredApp (int clientOS, int clientDeviceYear){
+        if (clientOS == 0 && clientDeviceYear >= 2015){
+            System.out.println("Установите приложение для iOS по ссылке:");
+        } else if (clientOS == 0 && clientDeviceYear < 2015){
+            System.out.println("Установите облегченную версию приложение для iOS по ссылке:");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015){
+            System.out.println("Установите приложение для Android по ссылке:");
+        } else if (clientOS == 1 && clientDeviceYear < 2015){
+            System.out.println("Установите облегченную версию приложение для Android по ссылке:");
+        } else {
+            System.out.println("Версия ОС не определена!");
+        }
+    }
+    //Lesson_10 HomeWork_3
+    public static void printDeliveryTime (int deliveryDistans){
+        if (deliveryDistans < 20 && deliveryDistans >= 0){
+            System.out.println("Для доставки потребуется один день.");
+        } else if (deliveryDistans >= 20 && deliveryDistans < 60){
+            System.out.println("Для доставки потребуется два дня.");
+        } else if (deliveryDistans >= 60 && deliveryDistans <= 100){
+            System.out.println("Для доставки потребуется три дня.");
+        } else {
+            System.out.println("Так далеко не доставляем!");
+        }
+    }
     public static void main(String[] args) {
+        //Test Lesson_10 HomeWork_1
+        System.out.println();
+        System.out.println("//Test Lesson_10 HomeWork_1");
+        int year = 2024;
+        printLeapYear(year);
 
-        //Lesson_9 HomeWork 1
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        //Test Lesson_10 HomeWork_2
+        System.out.println();
+        System.out.println("//Test Lesson_10 HomeWork_2");
+        byte clientOS = 0;
+        int clientDeviceYear = 2013;
+        printRequiredApp(clientOS, clientDeviceYear);
 
-        //Lesson_9 HomeWork 1
-        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        //Test Lesson_10 HomeWork_3
+        System.out.println();
+        System.out.println("//Test Lesson_10 HomeWork_3");
+        int distans = 80;
+        printDeliveryTime(distans);
 
-        //Lesson_9 HomeWork 1
-        firstName = "Семён";
-        middleName = "Семёнович";
-        lastName = "Иванов";
-        fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println("Данные Ф. И. О. сотрудника — " + fullName.replace('ё', 'е'));
     }
 }
